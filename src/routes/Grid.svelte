c3a99863c091e09968f8c7ad8f1dc52d8efa3eef
Tripouille <jmichel.gambard@gmail.com>
2023-09-28 21:16:27 +0200

    setup grid logic

--- src/routes/Grid.svelte
+++ src/routes/Grid.svelte
@@ -1,22 +1,49 @@
-<script lang='ts'>
-	import Square from "./Square.svelte";
+<script lang="ts">
+	import Square from './Square.svelte';
 
-    export let grid: string[];
+	export let grid: string[];
+	let foundedSquareIndexes: number[] = [];
+	let flippedSquareIndexes: number[] = [];
+	let timeoutID: number;
+
+	function flipSquare(index: number) {
+		if (flippedSquareIndexes.length >= 2) {
+			clearTimeout(timeoutID);
+			flippedSquareIndexes = [];
+		}
+
+		flippedSquareIndexes = flippedSquareIndexes.concat(index);
+
+		if (flippedSquareIndexes.length === 2) {
+			const [firstIndex, secondIndex] = flippedSquareIndexes;
+			if (grid[firstIndex] === grid[secondIndex]) {
+				foundedSquareIndexes = foundedSquareIndexes.concat(flippedSquareIndexes);
+			}
+			timeoutID = setTimeout(() => {
+				flippedSquareIndexes = [];
+			}, 1000);
+		}
+	}
 </script>
 
 <div class="grid">
-    {#each grid as emoji}
-        <Square {emoji} />
-    {/each}
+	{#each grid as emoji, i}
+		<Square
+			{emoji}
+			on:click={() => flipSquare(i)}
+			flipped={flippedSquareIndexes.includes(i)}
+			found={foundedSquareIndexes.includes(i)}
+		/>
+	{/each}
 </div>
 
 <style>
-    .grid {
-        display: grid;
-        grid-template-columns: repeat(4, 1fr);
-        grid-template-rows: repeat(4, 1fr);
-        gap: 0.2rem;
-        height: 100%;
-        padding: 0.2rem;
-    }
+	.grid {
+		display: grid;
+		grid-template-columns: repeat(4, 1fr);
+		grid-template-rows: repeat(4, 1fr);
+		gap: 0.2rem;
+		height: 100%;
+		padding: 0.2rem;
+	}
 </style>
--- src/routes/Square.svelte
+++ src/routes/Square.svelte
@@ -1,17 +1,30 @@
 <script lang="ts">
 	export let emoji: string;
+	export let flipped: boolean;
+	export let found: boolean;
 </script>
 
-<div class="square">
+{@debug flipped}
+<button class="square" class:flipped class:found disabled={found} on:click>
 	{emoji}
-</div>
+</button>
 
 <style>
 	.square {
-		background-color: lightcyan;
+		background-color: white;
 		border: 1px solid gray;
 		display: grid;
 		place-items: center;
 		font-size: 4rem;
+		cursor: pointer;
+	}
+
+	.flipped {
+		background-color: lemonchiffon;
+	}
+
+	.found {
+		background-color: lightgreen;
+		cursor: default;
 	}
 </style>
