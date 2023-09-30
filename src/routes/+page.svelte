1fba25a228e01729a4749bd8c65ee57ae2142ac5
Tripouille <jmichel.gambard@gmail.com>
2023-09-30 18:44:20 +0200

    end

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,28 @@
-<script>
-	import Game from "./Game.svelte";
-    import '../styles.css';
+<script lang="ts">
+	import '../styles.css';
+	import Game from './Game.svelte';
+	import Modal from './Modal.svelte';
+
+	let state: 'waiting' | 'playing' | 'paused' | 'won' | 'lost' = 'paused';
 </script>
 
 <Game />
+
+{#if state !== 'playing'}
+	<Modal>
+		<h1><span>Poke</span>match</h1>
+		{#if state === 'waiting'}
+			<h2>Choose a level</h2>
+		{:else if state === 'paused'}
+			<h2>Paused</h2>
+		{:else if state === 'won' || state === 'lost'}
+			you {state} the game
+		{/if}
+
+		<div class="buttons">
+			{#if state === 'paused'}
+				<button on:click={() => (state = 'playing')}>Resume</button>
+			{/if}
+		</div>
+	</Modal>
+{/if}
---
+++ src/routes/Modal.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+</script>
+
+<div class="modal-background">
+	<div class="content">
+		<slot />
+	</div>
+</div>
+
+<style>
+	.modal-background {
+		position: fixed;
+		inset: 0;
+		backdrop-filter: blur(10px);
+		display: grid;
+		place-items: center;
+		z-index: 999;
+	}
+</style>
